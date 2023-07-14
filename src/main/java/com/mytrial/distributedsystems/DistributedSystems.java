35b3dc2d07eaf5cba5bf385dc6cc4e0e7e4d540d
Reinhard <buyabor@gmail.com>
2023-07-14 14:26:17 +0100

    Initial Commit: Distributed Systems with Java

---
+++ src/main/java/com/mytrial/distributedsystems/DistributedSystems.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mytrial.distributedsystems;
+
+/**
+ *
+ * @author reinh
+ */
+public class DistributedSystems {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
